e924324f43b8344178908e8b1f8590e2a9937a29
Manvel <Manvelnavoan@gmail.com>
2024-08-18 21:53:29 +0300

    AAD in lesson4/task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+int[,] matrix = new int[5, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        string value = Console.ReadLine();
+         matrix[i, j] = Convert.ToInt32(value);
+    }
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+         Console.Write($"{matrix[i, j] } ");
+    }
+    Console.WriteLine();
+}
+
+     
+   
